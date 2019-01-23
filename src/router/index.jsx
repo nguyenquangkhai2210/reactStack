ce98fb7f8c16d549f47bc653bdce081232a2eb26
nguyenquangkhai <nguyenquangkhai2210@gmail.com>
2019-01-23 08:52:57 +0700

    add middleware check rule admin user

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,8 +1,13 @@
 import loadable from 'react-loadable';
+import withAdminRouteComponent from "./withAdminRouteComponent";
+import withUserRouteComponent from "./withUserRouteComponent";
 
 const Login = loadable({ loader: () => import('../main/loginPage/Login.jsx'), loading: () => null });
 const AdminPage = loadable({ loader: () => import('../main/adminPage'), loading: () => null });
 const UserPage = loadable({ loader: () => import('../main/userPage'), loading: () => null });
+
+const withAdmin = withAdminRouteComponent("/");
+const withUser = withUserRouteComponent("/");
 
 const indexRoutes = [
     {
@@ -13,12 +18,12 @@ const indexRoutes = [
     {
         path: '/admin',
         name: 'Admin',
-        component: AdminPage
+        component: withAdmin(AdminPage)
     },
     {
         path: '/user',
         name: 'User',
-        component: UserPage
+        component: withUser(UserPage)
     },
     { path: '/', pathTo: '/login', name: 'Login', redirect: true }
 ];
